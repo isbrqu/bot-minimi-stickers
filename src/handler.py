608ed6345448daa76c9ec309a25cd1463899ce39
Isaac Bruno Quiroga <isbrqu@gmail.com>
2021-05-25 23:17:31 -0300

    aplica concurrencia en la descarga

--- src/handler.py
+++ src/handler.py
@@ -4,6 +4,7 @@ from io import BytesIO
 from slugify import slugify
 from telegram import Update, Bot
 from telegram.ext import CallbackContext
+from concurrent.futures import ThreadPoolExecutor
 from zipfile import ZipFile
 import config
 import os
@@ -11,8 +12,7 @@ import os
 bot = Bot(config.token)
 
 def transform_sticker(sticker):
-    sticker_bytes = sticker.get_file().download_as_bytearray()
-    sticker_buffer = BytesIO(sticker_bytes)
+    sticker_buffer = BytesIO(sticker)
     image = Image.open(sticker_buffer)
     image = ImageOps.scale(image, .5)
     size = (521, image.height)
@@ -29,14 +29,23 @@ def sticker(update: Update, context: CallbackContext):
     user = update.effective_user
     msg = update.effective_message
     sticker_set = bot.get_sticker_set(msg.sticker.set_name)
+    update.message.reply_text('descargando...')
+    logger.info('download')
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        stickers = executor.map(
+            lambda sticker: sticker.get_file().download_as_bytearray(),
+            sticker_set.stickers
+        )
+    update.message.reply_text('comprimiendo...')
+    logger.info('compressing')
     zip_file_buffer = BytesIO()
-    update.message.reply_text('descargando imagenes...')
     with ZipFile(zip_file_buffer, mode='w') as zip_file:
-        for index, sticker in enumerate(sticker_set.stickers):
-            logger.info(f'descargando: {index}.webp')
+        for index, sticker in enumerate(stickers):
             image = transform_sticker(sticker)
-            logger.info(f'guardando')
             zip_file.writestr(f'{index}.png', image)
+    logger.info('compressed')
+    update.message.reply_text('comprimido')
+    update.message.reply_text('upload...')
     zip_file_name = f'{slugify(sticker_set.name)}.zip'
     update.message.reply_document(zip_file_buffer.getvalue(), zip_file_name)
     update.message.reply_text('se descargaron las imagenes las imagenes')
